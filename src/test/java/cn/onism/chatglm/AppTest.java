f34b6353c6a7b0f6b2e8068607a395870ae53baf
xdznHexin <2387740264@qq.com>
2024-04-11 22:45:19 +0800

    fix：更新测试代码

--- src/test/java/cn/onism/chatglm/AppTest.java
+++ src/test/java/cn/onism/chatglm/AppTest.java
@@ -11,7 +11,8 @@ import cn.onism.chatglm.model.ChatResponse;
 import cn.onism.chatglm.session.ChatGLMSession;
 import cn.onism.chatglm.session.Configuration;
 import cn.onism.chatglm.session.defaults.DefaultChatGLMSessionFactory;
-import cn.onism.chatglm.utils.EncryptUtils;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -23,13 +24,21 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.Serial;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+
 
 /**
  * 单元测试
  */
 @Slf4j
+@SuppressWarnings("all")
 public class AppTest {
 
     private ChatGLMSession chatGLMSession;
@@ -53,34 +62,7 @@ public class AppTest {
         CountDownLatch countDownLatch = new CountDownLatch(1);
 
         // 入参；模型、请求信息
-        ChatRequest request = new ChatRequest();
-        request.setModel(Model.GLM_3_TURBO); // chatGLM_6b_SSE、chatglm_lite、chatglm_lite_32k、chatglm_std、chatglm_pro
-        request.setIncremental(false);
-        request.setIsCompatible(true); // 是否对返回结果数据做兼容，24年1月发布的 GLM_3_5_TURBO、GLM_4 模型，与之前的模型在返回结果上有差异。开启 true 可以做兼容。
-        // 24年1月发布的 glm-3-turbo、glm-4 支持函数、知识库、联网功能
-        request.setTools(new ArrayList<>() {
-
-            @Serial
-            private static final long serialVersionUID = -7988151926241837899L;
-
-            {
-                add(ChatRequest.Tool.builder()
-                        .type(ChatRequest.Tool.Type.WEB_SEARCH)
-                        .webSearch(ChatRequest.Tool.WebSearch.builder().enable(true).searchQuery("Java快速排序").build())
-                        .build());
-            }
-        });
-        request.setPrompt(new ArrayList<>() {
-            @Serial
-            private static final long serialVersionUID = -7988151926241837899L;
-
-            {
-                add(ChatRequest.Prompt.builder()
-                        .role(Role.USER.value())
-                        .content("帮我写一个Java快速排序")
-                        .build());
-            }
-        });
+        ChatRequest request = getChatRequest();
 
         // 请求
         chatGLMSession.completions(request, new EventSourceListener() {
@@ -116,19 +98,101 @@ public class AppTest {
         }
     }
 
-    @Test
-    public void testApp(){
+    @NotNull
+    private static ChatRequest getChatRequest() {
+        ChatRequest request = new ChatRequest();
+        request.setModel(Model.GLM_3_TURBO); // chatGLM_6b_SSE、chatglm_lite、chatglm_lite_32k、chatglm_std、chatglm_pro
+        request.setIncremental(false);
+        request.setIsCompatible(true); // 是否对返回结果数据做兼容，24年1月发布的 GLM_3_5_TURBO、GLM_4 模型，与之前的模型在返回结果上有差异。开启 true 可以做兼容。
+        // 24年1月发布的 glm-3-turbo、glm-4 支持函数、知识库、联网功能
+        request.setTools(new ArrayList<>() {
 
+            @Serial
+            private static final long serialVersionUID = -7988151926241837899L;
+
+            {
+                add(ChatRequest.Tool.builder()
+                        .type(ChatRequest.Tool.Type.WEB_SEARCH)
+                        .webSearch(ChatRequest.Tool.WebSearch.builder().enable(true).build())
+                        .build());
+            }
+        });
+        request.setPrompt(new ArrayList<>() {
+            @Serial
+            private static final long serialVersionUID = -7988151926241837899L;
+
+            {
+                add(ChatRequest.Prompt.builder()
+                        .role(Role.USER.value())
+                        .content("帮我写一个Java快速排序")
+                        .build());
+            }
+        });
+        return request;
     }
 
+    /**
+     * 测试资料库功能
+     *
+     * @throws ExecutionException   执行异常
+     * @throws InterruptedException 中断异常
+     */
     @Test
-    public void testJson(){
-        Object object = 999L;
-        System.out.println(EncryptUtils.encrypt(JSONUtil.toJsonStr(object)));
+    public void testRetrieval() throws ExecutionException, InterruptedException {
+        ChatRequest request = new ChatRequest();
+        request.setModel(Model.GLM_3_TURBO);
+        request.setTools(new ArrayList<>(){
+            @Serial
+            private static final long serialVersionUID = 189288029302102L;
+            {
+                add(ChatRequest.Tool.builder()
+                        .type(ChatRequest.Tool.Type.RETRIEVAL)
+                        .retrieval(ChatRequest.Tool.Retrieval.builder()
+                                .knowledgeId("YOUR_KNOWLEDGE_ID")
+                                .promptTemplate("从文档\n\"\"\"\n{{knowledge}}\n\"\"\"\n中找问题\n\"\"\"\n{{question}}\n\"\"\"\n的答案，找到答案使用文档语句同时结合自身知识回答问题，并且不告诉用户该信息来自文档，找不到答案就用自身知识回答并且不告诉用户该信息不是来自文档。\n不要复述问题，直接开始回答。")
+                                .build())
+                        .build()
+                );
+            }
+        });
+        request.setMessages(new ArrayList<>(){
+            @Serial
+            private static final long serialVersionUID = 1L;
+            {
+                add(
+                        ChatRequest.Prompt.builder()
+                                .role(Role.USER.value())
+                                .content("请回答这个问题")
+                                .build()
+                );
+            }
+        });
+        CompletableFuture<String> future = chatGLMSession.completions(request);
+        String response = future.get();
+        System.out.println(response);
     }
 
     @Test
     public void testException(){
         throw new CustomException(CodeEnum.FAIL);
     }
+
+    @Test
+    public void testToken(){
+        // 创建Token
+        Algorithm algorithm = Algorithm.HMAC256("121212".getBytes(StandardCharsets.UTF_8));
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("api_key", "121212");
+        payload.put("uid","1212121");
+        payload.put("exp", System.currentTimeMillis() + 10 * 60 * 1000L);
+        payload.put("timestamp", Calendar.getInstance().getTimeInMillis());
+        Map<String, Object> headerClaims = new HashMap<>();
+        headerClaims.put("alg", "HS256");
+        headerClaims.put("sign_type", "SIGN");
+        String token = JWT.create().withPayload(payload).withHeader(headerClaims).sign(algorithm);
+        System.out.println(JWT.decode(token).getPayload());
+        System.out.println(token);
+    }
+
+
 }
